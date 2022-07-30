9079605c5cd6226fe50b1ae31f01246e6576ec4c
Xiaokang Li <83854324+xiaokli@users.noreply.github.com>
2022-07-30 17:27:12 +0800

    Create 2022-07-30-about-sysnology-hack.m

---
+++ _posts/2022-07-30-about-sysnology-hack.m
@@ -0,0 +1,11 @@
+---
+layout: post
+title: "Share My Linux Setup For Java Development" 
+date:   2021-05-08 13:18:21 +0800
+categories: nas
+---
+
+> Wants to share some experience with setup a NAS for family usgae, last month I spent about 1,000 CNY bought some hardwares and build a NAS in my home network used for storing
+ documents, photos and films to watch from the MiTV.
+  
+ 
